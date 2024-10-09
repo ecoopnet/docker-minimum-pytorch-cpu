6a2a96cabf618726e782aaf2ad3d058c96183e8c
Mitsuhiro Inomata <inomata@tech-vein.com>
2024-10-09 18:57:38 +0900

    マルチCPUアーキテクチャ対応イメージにした

--- build.sh
+++ build.sh
@@ -6,16 +6,8 @@ docker image build . -t "$IMAGE_NAME"
 
 VERSION=python3.12.4-pytorch2.3.1
 
-build() {
-  IMAGE="$1"
-  ARCH="$2"
-  VERSION="$3"
-  TARGET="$IMAGE":"${ARCH}-$VERSION"
-  docker build --platform linux/"$ARCH" . -t "$TAG"
-  docker push "$TARGET"
-}
+TARGET="$IMAGE_NAME":"$VERSION"
+docker buildx build --platform linux/arm64,linux/amd64 -t "$TARGET" --push .
+# docker push "$TARGET"
 
-
-build "$IMAGE_NAME" amd64 "$VERSION"
-build "$IMAGE_NAME" arm64 "$VERSION"
 
