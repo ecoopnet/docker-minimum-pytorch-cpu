8ac3e919b24da347738f077e4c98fcac89d2877b
Mitsuhiro Inomata <inomata@tech-vein.com>
2024-10-09 18:48:47 +0900

    バージョン情報を付与

--- build.sh
+++ build.sh
@@ -3,8 +3,19 @@ set -x
 
 IMAGE_NAME=inom/pytorch-cpu
 docker image build . -t "$IMAGE_NAME"
-docker build --platform linux/amd64 . -t "$IMAGE_NAME"
 
-#docker tag "$APP_ID" "$GCR_URL"
-#docker push "$GCR_URL"
+VERSION=python3.12.4-pytorch2.3.1
+
+build() {
+  IMAGE="$1"
+  ARCH="$2"
+  VERSION="$3"
+  TARGET="$IMAGE":"${ARCH}-$VERSION"
+  docker build --platform linux/"$ARCH" . -t "$TAG"
+  docker push "$TARGET"
+}
+
+
+build "$IMAGE_NAME" amd64 "$VERSION"
+build "$IMAGE_NAME" arm64 "$VERSION"
 
